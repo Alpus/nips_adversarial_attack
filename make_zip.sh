1aefd566f11ea951327a4f7393905f715c07086d
Alexander Pushin <work@apushin.com>
2017-09-30 22:52:14 +0300

    Add license

--- make_zip.sh
+++ make_zip.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 cd submit
-zip -r ../submit_${1:-'untagged'}.zip ./* -x "*__pycache__*" -x *.swp
-
+zip -r ../submit_${1:-'untagged'}.zip ./* ../LICENSE -x "*__pycache__*" -x *.swp
